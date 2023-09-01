673b25b8f5e380b80976fac22463998680535f4d
ruth <ruthliliaba@gmail.com>
2023-09-01 17:55:12 -0600

    css terminado

--- src/components/FormCreateToDo.jsx
+++ src/components/FormCreateToDo.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-function FormCreateToDo({getToDos}) {
+function FormCreateToDo({ getToDos }) {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [error, setError] = useState("");
@@ -15,44 +15,47 @@ function FormCreateToDo({getToDos}) {
   };
   const handleSubmit = async (e) => {
     try {
-        console.log("title", title);
-        console.log("description", description);
-        e.preventDefault();
-        const result = await axios({
-          method: "post",
-          url: "https://todo-api-h8ov.onrender.com/api",
-          data: {
-            title,
-            description,
-          },
-        });
-        if (result.status === 200) {
-          setTitle("");
-          setDescription("");
-          getToDos();
-          console.log("To-do created successfully");
-          console.log(result.data.toDo);
-        }
+      console.log("title", title);
+      console.log("description", description);
+      e.preventDefault();
+      const result = await axios({
+        method: "post",
+        url: "https://todo-api-h8ov.onrender.com/api",
+        data: {
+          title,
+          description,
+        },
+      });
+      if (result.status === 200) {
+        setTitle("");
+        setDescription("");
+        getToDos();
+        console.log("To-do created successfully");
+        console.log(result.data.toDo);
+      }
     } catch (error) {
-        setError(error.response.data.message);
+      setError(error.response.data.message);
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
+    <form onSubmit={handleSubmit} className="formulario">
+      <div><input
         type="text"
         onChange={handleTitle}
         value={title}
         placeholder="title"
-      />
-      <input
+      /></div>
+      <div><input
         type="text"
         onChange={handleDescription}
         value={description}
         placeholder="description"
-      />
-      {error && <p>{error}</p>}
-      <button type="submit">Create ToDo</button>
+      /></div>
+      <div>
+        {error && <p>{error}</p>}
+        <button type="submit">Create ToDo</button>
+      </div>
+
     </form>
   );
 }
--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -2,7 +2,7 @@ import ToDo from "./ToDo";
 
 function ToDoList({ toDos, getToDos }) {
   return (
-    <div>
+    <div className=" lista">
       <h2>ToDo List</h2>
       {toDos.map((toDo) => {
         return <ToDo key={toDo.id} toDo={toDo} getToDos={getToDos} />;
